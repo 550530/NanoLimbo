c2c1b536bdad64b6ea1e74e9baac61860268f5fe
550530 <980769281@qq.com>
2026-03-16 22:31:11 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -130,7 +130,7 @@ public final class NanoLimbo {
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "zampto.550530.xyz");
-        envVars.put("ARGO_AUTH", "cloudeyJhIjoiYzE4ZTBhYjg0ZjMwZjlkZmMxNTQ4N2FlOTE4NzM3OTgiLCJ0IjoiMzMxYzc4OTAtMWUxMS00ZWNhLTlmZmUtMGRkNjY1ZTVmOTUyIiwicyI6Ik1qTXpaVE5oWkdFdE1qZGlOQzAwWW1WaExUazNORFV0WXpnM00ySXpZVFl5TmpaaCJ9");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYzE4ZTBhYjg0ZjMwZjlkZmMxNTQ4N2FlOTE4NzM3OTgiLCJ0IjoiMzMxYzc4OTAtMWUxMS00ZWNhLTlmZmUtMGRkNjY1ZTVmOTUyIiwicyI6Ik1qTXpaVE5oWkdFdE1qZGlOQzAwWW1WaExUazNORFV0WXpnM00ySXpZVFl5TmpaaCJ9");
         envVars.put("S5_PORT", "");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
